a8bef589d4b3ce95287ee710009a5ba2fc89735e
PeterRosinski <rosinski.piotr@gmail.com>
2018-04-19 14:21:46 +0200

    Moduł 15.2

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -3,11 +3,20 @@ package com.crud.tasks.domain;
 
 import lombok.Getter;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="tasks")
 public class Task {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
+    @Column(name="name")
     private String title;
+    @Column(name="description")
     private String content;
 }
