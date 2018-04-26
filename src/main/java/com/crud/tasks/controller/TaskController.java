ed94f142ac116253d96855e865ee11d3f2eb5cb0
PeterRosinski <rosinski.piotr@gmail.com>
2018-04-26 15:49:10 +0200

    Zadanie: Moduł 15.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -6,6 +6,7 @@ import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,9 +25,9 @@ public class TaskController {
         return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
 
-    @RequestMapping(method=RequestMethod.GET, value="getTask")
-    public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L,"test title", "test_content");
+    @RequestMapping(method=RequestMethod.GET, value="getTask", params = {"taskId"})
+    public TaskDto getTask(@RequestParam(value = "taskId") Long taskId) {
+        return taskMapper.mapToTaskDto(service.getTask(taskId));
     }
 
     @RequestMapping(method=RequestMethod.DELETE, value="deleteTask")
