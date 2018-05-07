b0f1b427757f5c7d7e50f377ebf0aa97ceb7f3a0
PeterRosinski <rosinski.piotr@gmail.com>
2018-05-07 22:19:35 +0200

    Moduł 15.3

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -37,8 +37,9 @@ public class TaskController {
     }
 
     @RequestMapping(method=RequestMethod.PUT, value="updateTask")
-    public TaskDto updateTask(TaskDto taskDto) {
-        return new TaskDto(1L,"Edited test title", "test content");
+    public TaskDto updateTask(@RequestBody TaskDto taskDto) {
+        return taskMapper.mapToTaskDto(service.saveTask(taskMapper.mapToTask(taskDto)));
+
     }
 
     @RequestMapping(method=RequestMethod.POST, value="createTask", consumes = APPLICATION_JSON_VALUE)
